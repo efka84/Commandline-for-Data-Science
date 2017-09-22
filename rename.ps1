faf0d5c8aebac504d3529bef10b58147edafcc64
daroox <32034038+daroox@users.noreply.github.com>
2017-09-22 12:42:20 +0200

    Update rename.ps1

--- rename.ps1
+++ rename.ps1
@@ -1,6 +1,7 @@
 
 # rename files recorded by caen digitizer
-
+# original file names: Average Trace100 Scanresult Step.dat
+#                       Scanresult Step 100.txt
 dir *.dat | Rename-Item -NewName {$_.name -replace "Average Trace",""}    # rename / remove "Average Trace" with nothing
 dir *.dat | Rename-Item -NewName {$_.name -replace "Scanresult Step",""}
 dir *.dat | Rename-Item -NewName {$_.name -replace " ",""}
